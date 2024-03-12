a4d7c706137825dbcaf8c5c485f277b49ebe5f35
“Farahrama” <rafarah507@gmail.com>
2024-03-12 06:17:30 -0700

    d

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,27 +1,2 @@
 #!/usr/bin/python3
-long_script = """The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"""
-short1 = long_script[:98]
-short2 = long_script[98:196]
-short3 = long_script[196:]
-
-print(short1 + short2 + short3 )
+import this
