058f3e5e7b3a6bdd8e2a5ba60a967fbf59b83109
Farahrama <rafarah507@gmail.com>
2024-04-02 23:34:50 +0200

    f

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
   from add_0 import add
-  a = abs(1)
-  b = abs(2)
-  result = add(a, b)
+  a = 1
+  b = 2
+  result = abs(add(a, b))
   print("{} + {} = {}".format(a, b, result))
 
