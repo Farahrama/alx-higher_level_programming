2d6024d0a8dc9ea37ba93bd74114932959b99b49
Farahrama <rafarah507@gmail.com>
2024-04-04 06:01:31 +0200

    g

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(10, 5, add(10, 5)))
+    print("{} - {} = {}".format(10, 5, sub(10, 5)))
+    print("{} * {} = {}".format(10, 5, mul(10, 5)))
+    print("{} / {} = {}".format(10, 5, div(10, 5)))
