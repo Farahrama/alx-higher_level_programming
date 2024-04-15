bc70ef1716cadcc21e407d4b10971c32a3d03a21
Farahrama <rafarah507@gmail.com>
2024-04-16 01:59:42 +0200

    l

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=None):
-    if len(my_list) >= 5:
+    if my_list is None:
+        return
+    if len(my_list) > 0:
         i = len(my_list) - 1
         while i >= 0:
             print("{:d}".format(my_list[i]))
