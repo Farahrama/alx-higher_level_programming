430fc2f9544ce01e3ab9f9c0650aef43bcd3d865
Farahrama <rafarah507@gmail.com>
2024-04-23 01:30:27 +0200

    k

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,6 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[:1] = None
+        return(len(sentence), sentence[:1])
     else:
         return (len(sentence), sentence[:1])
