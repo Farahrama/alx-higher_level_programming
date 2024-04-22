5fec6237f10f9c340dc7887a63c855e004bb5bec
Farahrama <rafarah507@gmail.com>
2024-04-23 01:32:20 +0200

    k

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[:1] = None
-        return(len(sentence), sentence[:1])
+        sentence[:1] = ""
+        return (len(sentence), sentence[:1])
     else:
         return (len(sentence), sentence[:1])
