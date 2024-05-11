87267f3df196b5dcbd83928d4d34d90d3ef941af
Farahrama <rafarah507@gmail.com>
2024-05-11 05:30:52 +0200

    f

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_l = []
     for i in my_list:
         if i == search:
-           new_l.append(replace)
+            new_l.append(replace)
         else:
             new_l.append(i)
     return new_l
