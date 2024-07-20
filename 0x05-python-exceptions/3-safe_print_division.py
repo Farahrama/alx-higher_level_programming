2dc4c85c0d70c04b0960690d56364591774a5a1a
“Farahrama” <rafarah507@gmail.com>
2024-07-20 19:23:59 +0300

    safe_print_division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,12 +3,8 @@ def safe_print_division(a, b):
     res = 0
     try:
         res = a / b
-        return (res)
     except ZeroDivisionError:
         res = None
     finally:
-        if res != 0.0:
             print("Inside result: {}".format(res))
-        else:
-            print("Inside result: {}".format(None))
     return (res)
