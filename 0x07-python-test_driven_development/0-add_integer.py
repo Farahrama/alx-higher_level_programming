040c0dc6484adc152d1b47d49399082ae751e613
“Farahrama” <rafarah507@gmail.com>
2024-08-01 00:17:38 +0300

    f

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""docs"""
 def add_integer(a, b=98):
     """
     Adds two integers or floats after converting them to integers.
@@ -13,9 +14,13 @@ def add_integer(a, b=98):
     Raises:
     TypeError: If a or b are not integers or floats.
     """
-    if a is None or (type(a) is not int and type(a) is not float):
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if a is None or (type(a) not in [int, float]):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     result = a + b
     if result == float('inf') or result == -float('inf'):
