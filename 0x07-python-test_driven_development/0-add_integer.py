47c5dff0448db9366eee2e2b0de5c5f8d7afb648
“Farahrama” <rafarah507@gmail.com>
2024-07-31 21:43:46 +0300

    g

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -22,9 +22,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if isinstance(b, float) and (b != b):
         raise TypeError("b must be an integer")
-    try:
-        a = int(a)
-        b = int(b)
-    except OverflowError:
-        raise OverflowError("float overflow")
+     if (a > 0 and b > 0 and a + b > sys.maxsize) or \
+     (a < 0 and b < 0 and a + b < sys.minsize):
+        raise OverflowError("Input too large to convert to integer"
     return a + b
