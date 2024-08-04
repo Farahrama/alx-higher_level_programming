4549d087eb343cd649158088c824cd5ca4fae0c9
“Farahrama” <rafarah507@gmail.com>
2024-08-04 19:58:36 +0300

    d

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,6 +15,8 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
