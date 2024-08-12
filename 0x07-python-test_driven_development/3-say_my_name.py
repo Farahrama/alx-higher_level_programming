809245f9b2ec8c422164a4589e62bcc64f46b2e0
“Farahrama” <rafarah507@gmail.com>
2024-08-12 03:36:02 +0300

    e

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this is 3-say_my_name.py Module for print fist and second name
+suplies one function, def say_my_name(first_name, last_name="")
+"""
+def say_my_name(first_name, last_name=""):
+    """function that prints My name is <first name> <last name> """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(first_name + " " + last_name)
