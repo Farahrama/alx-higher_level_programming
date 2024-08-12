290c19df8c321d01ca042914df454c101d99c007
“Farahrama” <rafarah507@gmail.com>
2024-08-12 20:39:38 +0300

    h

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print(first_name + " " + last_name)
+    print("My name is {} {}".format(first_name, last_name))
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
