d165f2439ad0433660e5d6bc852106a833cf5a47
“Farahrama” <rafarah507@gmail.com>
2024-08-14 02:38:42 +0300

    f

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this is 5-text_indentation.py Module for  prints a text with 2 new lines after each of these characters: ., ? and :
+    supplies one function, def text_indentation(text)"""
+def text_indentation(text):
+    """function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    word = ""
+    for char in text:
+        word += char
+        if char in [".", "?", ":"]:
+            print(word.strip() + "\n")
+            word = ""
+    if word:
+        print(word.strip())
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('tests/5-text_indentation.txt')
+
