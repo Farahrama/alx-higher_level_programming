61508bafe4134ceed2a95ea54ad517a1fc040d8f
“Farahrama” <rafarah507@gmail.com>
2024-08-14 17:10:55 +0300

    d

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,7 @@ This is the "5-test_indentation" module.
 The 5-text_indentation module supplies one function, text_indentation(text).
 """
 
+
 def text_indentation(text):
     """Splits a text into lines along "?", ":", "." followed by 2 new lines"""
     if not isinstance(text, str):
@@ -24,7 +25,8 @@ def text_indentation(text):
             else:
                 print(char, end="")
 
+
+
 # Example usage
 if __name__ == "__main__":
     text_indentation("Holberton. School? How are you: John")
-
