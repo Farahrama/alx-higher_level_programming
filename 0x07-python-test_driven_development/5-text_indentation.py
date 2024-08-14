48fdfde75af1f4d597a9a2c9ce0b26459bf06d6e
“Farahrama” <rafarah507@gmail.com>
2024-08-14 05:59:18 +0300

    j

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""this is 5-text_indentation.py Module for  prints a text with 2 new lines after each of these characters: ., ? and :
-    supplies one function, def text_indentation(text)"""
+"""
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
+"""
+
 def text_indentation(text):
-    """function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """Splits a text into lines along "?", ":", "." followed by 2 new lines"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    word = ""
+
+    flag = 0
     for char in text:
-        word += char
-        if char in [".", "?", ":"]:
-            print(word.strip() + "\n")
-            word = ""
-    if word:
-        print(word.strip())
+        if flag == 0:
+            if char == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if char in ['?', '.', ':']:
+                print(char)
+                print()
+                flag = 0
+            else:
+                print(char, end="")
 
+# Example usage
+if __name__ == "__main__":
+    text_indentation("Holberton. School? How are you: John")
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile('tests/5-text_indentation.txt')
