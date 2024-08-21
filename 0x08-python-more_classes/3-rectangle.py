c72f65f253f38b8aaefea4c7d0948075b44853c2
“Farahrama” <rafarah507@gmail.com>
2024-08-21 05:09:16 +0300

    delete __repr__ function

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """ defines a rectangle and str() should print the rectangle with the character # """
+    """str() should print the rectangle with the character # """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -44,6 +44,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(("#" * self.__width for _ in range(self.__height)))
-
-    def __repr__(self):
-        return f"Rectangle(width={self.__width}, height={self.__height})"
