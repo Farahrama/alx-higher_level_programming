88669b2d3299977a3c6c9df63920faf77c5eba79
“Farahrama” <rafarah507@gmail.com>
2024-09-02 01:01:07 +0300

    blankline before function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """lookup function that returns the list of available attributes and methods"""
+
 def lookup(obj):
     """return the list of available attributes and methods"""
     return dir(obj)
