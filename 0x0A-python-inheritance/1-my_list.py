b93c2a2ebfd352ba0478bce63c2652df472c4488
“Farahrama” <rafarah507@gmail.com>
2024-09-02 07:49:56 +0300

    push

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """class MyList that inherits from list"""
+
 class MyList(list):
     """Mylist class"""
-
 
     def print_sorted(self):
         """Print the list in sorted order"""
