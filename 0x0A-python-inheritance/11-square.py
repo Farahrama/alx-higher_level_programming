32cebc236162db5853551e4dff295f2baf929a84
“Farahrama” <rafarah507@gmail.com>
2024-09-04 05:32:34 +0300

    no whitespaces

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,8 +7,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ that inherits from Rectangle (9-rectangle.py)"""
     def __init__(self, size):
-        super().integer_validator("size", size)        
-        super().__init__(size, size)   
+        super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
