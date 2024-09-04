7bea9296a8156b19f32e30441003aaf6cc5fc1e8
“Farahrama” <rafarah507@gmail.com>
2024-09-04 05:14:15 +0300

    push

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,5 +13,8 @@ class Square(Rectangle):
         self.__height = size
         self.__size = size    
 
+    def area(self):
+        return self.__size * self.__size
+
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
