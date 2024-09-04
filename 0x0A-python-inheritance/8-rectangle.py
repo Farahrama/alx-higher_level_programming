30d4c2e347c120a4a517825f298efe4be67711a3
“Farahrama” <rafarah507@gmail.com>
2024-09-04 03:32:29 +0300

    push

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """class Rectangle that inherits from BaseGeometry (7-base_geometry.py)."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """that inherits from BaseGeometry (7-base_geometry.py)."""
     def __init__(self, width, height):
