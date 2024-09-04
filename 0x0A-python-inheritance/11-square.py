8220ffd3688b079a25f38ef68ef9f1c44624f049
“Farahrama” <rafarah507@gmail.com>
2024-09-04 04:50:47 +0300

    push

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,10 +4,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 (task based on 10-square.py)."""
 
 class Square(Rectangle):
-
+    """ that inherits from Rectangle (9-rectangle.py)"""
     def __init__(self, size):
         super().__init__(size, size)
         super().integer_validator("size", size)
         self.__width = size
         self.__height = size
         self.__size = size    
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
