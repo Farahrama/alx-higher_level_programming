18b201d142cfef55d22122b8cd2c57cf0a0ac9c0
“Farahrama” <rafarah507@gmail.com>
2024-09-04 04:28:41 +0300

    blankline before __init__

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """that inherits from BaseGeometry (7-base_geometry.py)."""
+
     def __init__(self, width, height):
         super().integer_validator("width", width)
         self.__width = width
