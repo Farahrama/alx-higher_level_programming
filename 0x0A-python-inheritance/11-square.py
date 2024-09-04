575647efbad78f579b2fcd9fbe7ba04ae2b10ecc
“Farahrama” <rafarah507@gmail.com>
2024-09-04 04:52:23 +0300

    push

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """class Square that inherits from Rectangle (9-rectangle.py).
 (task based on 10-square.py)."""
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """ that inherits from Rectangle (9-rectangle.py)"""
