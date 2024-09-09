2da9880bc3ed70ceab7beb449316d3f7bd86e05b
“Farahrama” <rafarah507@gmail.com>
2024-09-09 17:02:18 +0300

    push

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
         with open(filename, encoding="UTF8") as file:
             print(file.read(), end="")
     except FileNotFoundError:
-        print(f"No such file or directory: 'file_nop'")
+        print(f"[FileNotFoundError] [Errno 2] No such file or directory: 'file_nop'")
