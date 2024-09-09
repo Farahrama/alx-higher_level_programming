822a8bbb6237347b0f1355bd77c2f1643e7fbb19
“Farahrama” <rafarah507@gmail.com>
2024-09-10 02:43:28 +0300

    push

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 def write_file(filename="", text=""):
     """returns the number of characters written:"""
     try:
-        with open(filename, encoding="UTF8") as file:
+        with open(filename, "w",encoding="UTF8") as file:
             print(file.write(text), end="")
     except FileNotFoundError:
         print(f"[FileNotFoundError] [Errno 2] No such file or directory: "
