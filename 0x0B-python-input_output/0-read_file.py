35bf42c6a30ed9b439e624568c205fc4b11d5d29
“Farahrama” <rafarah507@gmail.com>
2024-09-09 05:08:20 +0300

    push

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """function that reads a text file (UTF8) and prints it to stdout:"""
+
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, encoding="UTF8") as file:
