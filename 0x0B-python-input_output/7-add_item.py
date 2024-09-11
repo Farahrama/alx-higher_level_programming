9b85ab31caff889bff7fd864049d97e6a3af1ad9
“Farahrama” <rafarah507@gmail.com>
2024-09-11 03:32:02 +0300

    push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,11 @@ import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-filename = "add_item.json"
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    filename = "add_item.json"
+    if os.path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
