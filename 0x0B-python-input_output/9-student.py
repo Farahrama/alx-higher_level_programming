44197ee2fc5ca74ff966e67b6fc8c736047a4acd
“Farahrama” <rafarah507@gmail.com>
2024-09-12 03:28:01 +0300

    push

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,12 +5,10 @@
 class Student:
     """defines a Student"""
 
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """ that retrieves a dictionary representation of a Student"""
