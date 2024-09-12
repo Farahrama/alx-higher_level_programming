c2dafd7a0e9c22a5fd575da3a7c5c81da686d283
“Farahrama” <rafarah507@gmail.com>
2024-09-12 04:55:21 +0300

    push

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """class Student that defines a student by: (based on 9-student.py)"""
-Student = __import__('9-student').Student
 
-def to_json(self, attrs=None):
-    """based on 9-student.py"""
-    if attrs is None:
-        return self.__dict__
-    else:
-        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+class Student:
+    """defines a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """based on 9-student.py"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs\
+                     if hasattr(self, attr)}
