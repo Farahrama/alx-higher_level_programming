30cb91df72107843c4a0965f105970717e7d8837
“Farahrama” <rafarah507@gmail.com>
2024-09-12 07:12:03 +0300

    push

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""class Student that defines a student by: (based on 9-student.py)"""
+
+
+class Student:
+    """defines a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """based on 9-student.py"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                return setattr(self, key, value)
