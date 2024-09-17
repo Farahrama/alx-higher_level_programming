8e95a9bd382184a8288d182719f6568b9536b75c
“Farahrama” <rafarah507@gmail.com>
2024-09-17 08:22:55 +0300

    push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,43 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int and value <= 0:
+            raise ValueError("width must be a positive integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int and value <= 0:
+            raise ValueError("height must be a positive integer")
+        self.__height = value
+
+    @property
+    def x(self, value):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int and value <= 0:
+            raise ValueError("x must be a non-negative integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int and value <= 0:
+            raise ValueError("y must be a non-negative integer")
+        self.__y = value
