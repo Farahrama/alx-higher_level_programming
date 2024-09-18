0f5b6df674597315f8a8c1bb0ceae33f9a083451
“Farahrama” <rafarah507@gmail.com>
2024-09-18 08:43:11 +0300

    push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,6 +69,7 @@ class Rectangle(Base):
         """that prints in stdout the Rectangle instance with the character #"""
         for i in range(self.__height):
             print((self.__width) * "#")
+
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[Rectangle] ({}) {}/{} - {}/{}"\
