f5cd5a936b35b36c6c0538c7ef2781b6a207bf64
“Farahrama” <rafarah507@gmail.com>
2024-09-18 07:05:46 +0300

    push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """class Rectangle that inherits from Base"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -60,3 +60,5 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+    def area(self):
+        return self.__width * self.__height
