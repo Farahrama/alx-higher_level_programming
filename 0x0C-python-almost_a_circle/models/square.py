9c2e738786926bc36f1e93d098694d110f66c40a
“Farahrama” <rafarah507@gmail.com>
2024-09-19 11:12:44 +0300

    push

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,11 +2,12 @@
 """ class Square that inherits from Rectangle"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)        
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -20,3 +21,6 @@ class Square(Rectangle):
             raise ValueError("size must be > 0")
         self.width = value
         self.height = value
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.size)
