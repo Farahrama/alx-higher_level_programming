6f9f9246df8df2f8f1b93d7d9ed4cc75ca2858ad
“Farahrama” <rafarah507@gmail.com>
2024-09-19 11:02:06 +0300

    push

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" class Square that inherits from Rectangle"""
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    """inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)        
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.width = value
+        self.height = value
