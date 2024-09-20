37430df5432756ddefed0b54c02e50923b8f309f
“Farahrama” <rafarah507@gmail.com>
2024-09-20 08:01:11 +0300

    @staticmethod before the method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,9 +14,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """that returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries ) <= 0:
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
             return "[]"
         json_string = json.dumps(list_dictionaries)
         return json_string
