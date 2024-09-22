696e3b8c17857c0b598de28822b5ce6148fef98e
“Farahrama” <rafarah507@gmail.com>
2024-09-22 08:08:38 +0300

    push

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pytho3
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_id_auto_assignment(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
+        base2 = Base()
+        self.assertEqual(base2.id, 2)
+    def test_id_manual_assignment(self):
+        base = Base(50)
+        self.assertEqual(base.id, 50)
+    def to_json_string_None_list(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+    def to_json_string_empty_list(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
+    def to_json_string_valid(self):
+        dictionary = [{"id": 12}]
+        self.assertEqual(Base.to_json_string(dictionary), [{"id": 12}])
